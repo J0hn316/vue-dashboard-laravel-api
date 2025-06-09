37c268bef1860b9c9ebf3007457c3aa478605dbe
John Wrimene <jcordell316@gmail.com>
2025-06-09 11:07:06 -0400

    Added relationships between User, Post, and Todo models

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
     protected $fillable = ['title', 'description', 'completed', 'user_id'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
