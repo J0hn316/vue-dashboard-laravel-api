2dd3787296e8a9b3b867bf26f5164e94529bab2f
John Wrimene <jcordell316@gmail.com>
2025-06-10 20:25:54 -0400

    next push

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\PostController;
 
-// Note: the apiResource handles CRUD operations GET, POST, PUT, DELETE
-// So you don't need to define each route individually
-
-// To-do Route
-Route::apiResource('todos', TodoController::class);
-
-// User routes
-Route::apiResource('users', UserController::class);
-
-// Post routes
-Route::apiResource('posts', PostController::class);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('posts', PostController::class);
+    Route::apiResource('todos', TodoController::class);
+    Route::apiResource('users', UserController::class);
+});
